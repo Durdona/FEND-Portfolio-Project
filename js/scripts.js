df396d5090c45dc25816780e371ad73a41f6e85e
Durdona <holisihlos@gmail.com>
2018-07-10 20:22:24 -0400

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
-// background video settings
+ // Intro Page Opener 
+$().introtzikas({
+   line: '#F56991',
+   speedwidth: 2000, 
+   speedheight: 1000, 
+   bg: '#1080C9',
+   lineheight: 2
+});
+ 
+
+// Background Video Settings
 var video = document.getElementById('back_video').playbackRate = 1;
 
-		
- 
+
+// About Me Paragraph Animation 
+$('.tlt').textillate({ in: { effect: 'rollIn' } });
+
+
+
